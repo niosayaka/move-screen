567ea68d446c3daee21cda5c6973c9cc3af78f8b
niosayaka <niosayaka@gmail.com>
2018-05-22 20:02:01 +0900

    Initial Commit

---
+++ 画面遷移と値渡し/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  画面遷移と値渡し
+//
+//  Created by 丹尾 沙也花 on 2018/05/22.
+//  Copyright © 2018年 litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
